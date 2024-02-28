4cfdf04320b4218648ce3eb4eed57c5f7e2de634
Cameron Croteau <119271255+camcrow@users.noreply.github.com>
2024-02-28 15:27:31 -0600

    Add files via upload

---
+++ world/door.gd
@@ -0,0 +1,32 @@
+class_name Door
+extends Area2D
+
+var active = false
+
+@export_file("*.tscn") var new_level_path
+@export var connection : DoorConnection
+
+@onready var right_cast = $RightCast
+@onready var left_cast = $LeftCast
+
+func get_direction():
+	if left_cast.is_colliding():
+		return -1
+	if right_cast.is_colliding():
+		return 1
+	return 0
+
+func _physics_process(delta):
+	var player = MainInstances.player as Player
+	if not player is Player: return
+	if not active: return
+	if overlaps_body(player) and new_level_path:
+		var player_direction = sign(player.velocity.x)
+		var direction = get_direction()
+		if player_direction == direction:
+			Events.door_entered.emit(self)
+		
+
+
+func _on_timer_timeout():
+	active = true
---
+++ world/trigger.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+var is_active = true
+
+signal trigger_entered
+
+
+func _on_body_entered(body):
+	if not is_active: return
+	trigger_entered.emit()
